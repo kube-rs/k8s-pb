ccc3ac20ed36d7c60a03ffff85393e3352a00d0f
clux <sszynrae@gmail.com>
2024-06-08 15:29:00 +0100

    renovate gha self-running
    Signed-off-by: clux <sszynrae@gmail.com>

---
+++ .github/renovate-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# renovate: datasource=github-releases depName=clux/whyq
+ARG YQ_VER=0.10.2
+# renovate: datasource=github-releases depName=casey/just
+ARG JUST_VER=1.28.0
+# renovate: datasource=github-releases depName=BurntSushi/ripgrep
+ARG RG_VER=14.1.0
+# renovate: datasource=github-releases depName=sharkdp/fd
+ARG FD_VER=10.1.0
+# renovate: datasource=github-releases depName=chmln/sd
+ARG SD_VER=1.0.0
+
+apt update
+apt install -y curl git
+
+curl -sSL https://github.com/clux/whyq/releases/download/${YQ_VER}/yq-x86_64-unknown-linux-musl.tar.xz | tar xJ --strip-components=1 -C /usr/local/bin && \
+curl -sSL https://github.com/casey/just/releases/download/${JUST_VER}/just-${JUST_VER}-x86_64-unknown-linux-musl.tar.gz | tar xz -C /usr/local/bin && \
+curl -sSL https://github.com/BurntSushi/ripgrep/releases/download/${RG_VER}/ripgrep-${RG_VER}-x86_64-unknown-linux-musl.tar.gz | tar xz --strip-components=1 -C /usr/local/bin && \
+curl -sSL https://github.com/sharkdp/fd/releases/download/v${FD_VER}/fd-v${FD_VER}-i686-unknown-linux-musl.tar.gz | tar xz -C /usr/local/bin && \
+    ln -s /usr/local/bin/fd-v${FD_VER}-i686-unknown-linux-musl/fd /usr/local/bin/fd && \
+curl -sSL https://github.com/chmln/sd/releases/download/v${SD_VER}/sd-v${SD_VER}-x86_64-unknown-linux-musl > /usr/local/bin/sd && chmod +x /usr/local/bin/sd
+
+runuser -u ubuntu renovate
---
+++ .github/renovate-postupgrade.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+just protos
+just swagger
+just codegen
